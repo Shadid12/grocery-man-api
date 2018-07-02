d78996bf60f00d74a0006e574aae3bce8d69743c
shadid12 <shadidhaque2014@gmail.com>
2018-07-02 19:28:30 -0400

    added new cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import api from './api';
 import config from './config.json';
 
 const app = express();
 
 // 3rd party middleware
+app.use(cors({
+	exposedHeaders: config.corsHeaders
+}));
 
 app.use(bodyParser.json({
 	limit : config.bodyLimit
